a2af294258784dd66086d6f6a32ee449f0b0830f
NandaAmaliaPutri <nandaamaliaputri@mail.ugm.ac.id>
2024-10-21 16:00:20 +0700

    Koneksi PHP dengan database

---
+++ success.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+    <h2>Data Berhasil Dimasukkan</h2>
+    <p>Data Anda telah berhasil dimasukkan ke dalam sistem.</p>
+    
+    <!-- Tautan untuk kembali ke halaman form -->
+    <a href="index.html">
+        <button>Kembali ke Form</button>
+
+    <!-- Tambahan tombol untuk kembali ke tabel -->
+    <br><br>
+    <a href="latihan1.php">
+        <button>Kembali ke Tabel</button>
+    </a>
+</body>
+</html>
